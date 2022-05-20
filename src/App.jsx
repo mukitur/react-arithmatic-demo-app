e016d4dd7fc5b49bee7886e22cbdb1770684a140
mukitur <mukit.inway@gmail.com>
2022-05-20 17:29:34 +0600

    react arithmatic demo without refactor

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/**
+ * DONE: Handle user input fields
+ * DONE: Handle operations
+ * DONE: Handle a list of histories
+ * DONE: Render history list
+ * DONE: Restore the history
+ */
+
+import Home from "./compopnents/Home/Home";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
